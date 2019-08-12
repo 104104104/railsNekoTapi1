dda999bd0062ae49caf24e863520253603e2c50f
{104104104} <{rz344788@yahoo.co.jp}>
2019-08-12 10:34:46 +0900

    ログイン機能付きadminできた

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#adminのパスワード
+AdminUser.create!(email: 'rz344788@yahoo.co.jp', password: 'nekotapi', password_confirmation: 'nekotapi') if Rails.env.development?
